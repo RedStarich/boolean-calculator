eb94b8a6f52219a01e2720e837754c5df88b4f41
RedStarich <altairzhambyl@gmail.com>
2023-11-16 12:16:56 +0600

    simplification of minterms
    added simplification of minterms if there are two inverse elements of a single variable (i.e., a and A)

--- bool_calculator.cpp
+++ bool_calculator.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 
+string simplify_minterm(string a) {
+    set <char> s (a.begin(), a.end());
+    string b;
+    for (set<char>::iterator it=s.begin(); it!= s.end(); ++it) {
+        b.push_back(*it);
+    }
+
+    bool iszero = false;
+    for (int i=0; i<b.size(); i++) {
+        if (islower(b[i])) {
+            for (int j=0; j<b.size(); j++) {
+                if (toupper(b[i])==b[j]) {
+                    iszero = true;
+                }
+            }
+        }
+    }
+    if (iszero) {b = '0';}
+    return b;
+}
+
 int main() {
+    string res;
     int n;
     cout << "Enter number of variables"<<endl;
     cin >> n;
+    while ((n < 2)|(n>4)) {
+        cout << "Number of variables cannot be less than 2 or more than 4"<<endl;
+        cin >> n;
+    }
     char var[] = {'a', 'b', 'c', 'd'};
     string expression;
     for (int i=0; i<n; i++) {
         cout << "Enter expression using a, b, c, d "<< i+1<<endl;
-        cout << "To make AND statement, use *"<<endl;
         cout<< "To make OR statement, use +"<<endl;
-        cout << "To make NOT statement, use ' above the variable, i.e. a', b', c', d'"<<endl;
+        cout << "To make NOT statement, use ' above the variable, i.e. A, B, C, D'"<<endl;
         cout << "To finish expression, enter 'finish'"<<endl;
     }
     string tmp;
@@ -20,12 +46,20 @@ int main() {
     vector <string> oper;
     while (tmp != "finish") {
         cin >> tmp;
-        if ((tmp=="+")|(tmp=="*")) {
+        if (tmp =="finish") {break;}
+        if (tmp=="+") {
             oper.push_back(tmp);
         } else {
             minterm.push_back(tmp);
         }
     }
 
-    
+    for (int j=0; j<minterm.size(); j++)
+    {
+        cout << simplify_minterm(minterm[j])<<endl;
+        minterm[j] = simplify_minterm(minterm[j]);
+    }
+
+    cout << res;
+
 }
